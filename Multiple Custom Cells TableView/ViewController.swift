c6c209dfb25067b3c7855453be1e2cc52ce9bb26
yusufkaran <karan.yusuf.20@gmail.com>
2023-03-05 18:15:58 +0300

    Project Created

---
+++ Multiple Custom Cells TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multiple Custom Cells TableView
+//
+//  Created by Yusuf Karan on 5.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
